8baf47b72754aa484af164618edb8ab36a6414f1
Barti <bartosz.majkowski@wp.pl>
2025-03-18 22:40:42 +0100

    make handlers async

--- handlers.js
+++ handlers.js
@@ -1,49 +1,49 @@
-import DbConnector from "./dbConnector.js";
-
 class Handlers {
     constructor(db_connector) {
         this.db_connector = db_connector
     }
 
-    add_auction_handler(auction) {
-        this.db_connector.add_auction(auction, (err, result) => {
+    async add_auction_handler(auction) {
+        await this.db_connector.add_auction(auction, (err, result) => {
             if (err) console.error(err);
             else console.log(result);
         })
     }
 
-    add_offer_handler(offer, auction_id) {
-        this.db_connector.add_offer(offer, auction_id, (err, result) => {
+    async add_offer_handler(offer, auction_id) {
+        await this.db_connector.add_offer(offer, auction_id, (err, result) => {
             if (err) console.error(err);
             else console.log(result);
         })
     }
 
-    get_auction_handler(auction_id) {
-        this.db_connector.get_auction(auction_id, (err, result) => {
+    async get_auction_handler(auction_id) {
+        return await this.db_connector.get_auction(auction_id, (err, result) => {
             if (err) console.error(err);
             else console.log(result);
         })
     }
 
-    get_all_auction_offers_handler(auction_id) {
-        this.db_connector.get_all_auction_offers(auction_id, (err, result) => {
+    async get_all_auction_offers_handler(auction_id) {
+        return await this.db_connector.get_all_auction_offers(auction_id, (err, result) => {
             if (err) console.error(err);
             else console.log(result);
         })
     }
 
-    get_all_ended_auctions_handler() {
-        this.db_connector.get_all_ended_auctions((err, result) => {
+    async get_all_ended_auctions_handler() {
+        return await this.db_connector.get_all_ended_auctions((err, result) => {
             if (err) console.error(err);
             else console.log(result);
         })
     }
 
-    get_all_actual_auctions_handler() {
-        this.db_connector.get_all_actual_auctions((err, result) => {
+    async get_all_actual_auctions_handler() {
+        return await this.db_connector.get_all_actual_auctions((err, result) => {
             if (err) console.error(err);
             else console.log(result);
         })
     }
 }
+
+export default Handlers
