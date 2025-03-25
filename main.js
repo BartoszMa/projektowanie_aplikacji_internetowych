7ae5233d5a74d234622eb09d0812fa7515177a2b
Barti <bartosz.majkowski@wp.pl>
2025-03-25 12:17:51 +0100

    fix css

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import {router} from "./endpoints.js";
+import path from "path";
 
 const app = express()
 const port = 3000
 
-
+app.use(express.static(path.join(path.resolve(), 'public')));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
